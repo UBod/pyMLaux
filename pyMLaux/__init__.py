7e4e6e31dfbfcab17e340b82b27330520bbc18ac
UBod <ulrich@bodenhofer.com>
2021-09-13 11:53:37 +0200

    Update __init__.py

--- pyMLaux/__init__.py
+++ pyMLaux/__init__.py
@@ -1,2 +1,4 @@
 ## dummy file (but necessary for module!)
-from .functions import plot_2d_prediction
+from .functions import plot_2d_prediction, show_img, show_img_data, plot_history, \
+                       evaluate_classification_result, evaluate_regression_result, \
+                       read_MNIST, create_data_from_testimage
