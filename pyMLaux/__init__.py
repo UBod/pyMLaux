ee8755b12245e0a2e883656d5544927561240765
UBod <ulrich@bodenhofer.com>
2021-10-12 11:18:27 +0200

    Added function for plotting dendrogram

--- pyMLaux/__init__.py
+++ pyMLaux/__init__.py
@@ -1,4 +1,4 @@
 ## dummy file (but necessary for module!)
 from .functions import plot_2d_prediction, show_img, show_img_data, plot_history, \
                        evaluate_classification_result, evaluate_regression_result, \
-                       read_MNIST, create_data_from_testimage
+                       plot_dendrogram, read_MNIST, create_data_from_testimage
