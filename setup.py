d7b1d7edd4c2d80c280e664d269e7e769d77aa76
UBod <ulrich@bodenhofer.com>
2021-10-12 14:29:05 +0200

    Version number bumped to 0.0.2 after new function had been added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='pyMLaux',
-    version='0.0.1',
+    version='0.0.2',
     author='Ulrich Bodenhofer',
     author_email='ulrich@bodenhofer.com',
     description='Auxiliary functions for machine learning courses and projects',
